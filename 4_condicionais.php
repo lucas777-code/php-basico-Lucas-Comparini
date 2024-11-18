4360b491c2afa88be35a81439b8a4f372cf8ef0f
luquinhas <lucascomparini0@gmail.com>
2024-11-18 14:36:52 -0300

    Resolução exercicio 4

--- 4_condicionais.php
+++ 4_condicionais.php
@@ -1,32 +1,5 @@
 <?php
 
-// Digitar PHP (1º Aqui)
-
-?>
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <title>Página de Login</title>
-</head>
-<body>
-    <h2>Digite a senha para continuar:</h2>
-    <form method="post" action="">
-        <label for="senha">Senha:</label>
-        <input type="password" name="senha" required><br>
-        <button type="submit">Entrar</button>
-    </form>
-
-    <?php
-
-    // Digitar PHP (2º Aqui)
-
-    ?>
-</body>
-</html>
-
-<?php
-
 //Verificar os dados do formulário
 // Utilizando o método POST
 
